a8c33bea1e55745dfddaf2d392ff342232c12c2c
AlexSid <palach.orl@mail.ru>
2021-02-21 17:10:49 +0300

    исправлены баги, фрагмент отрабатывает

--- app/src/main/java/my/notesapp/DetailNoteActivity.java
+++ app/src/main/java/my/notesapp/DetailNoteActivity.java
@@ -11,19 +11,19 @@ import androidx.fragment.app.FragmentTransaction;
 
 public class DetailNoteActivity extends AppCompatActivity {
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.detail_activity);
 
+        if (savedInstanceState == null) {
+            Intent intent = getIntent();
+            Note note = (Note) intent.getSerializableExtra(DetailNoteFragment.ARG_INDEX);
+            DetailNoteFragment detailNoteFragment = DetailNoteFragment.newInstance(note);
 
-//            Intent intent = getIntent();
-//            Note note = (Note) intent.getSerializableExtra(DetailNoteFragment.ARG_INDEX);
-//            DetailNoteFragment detailNoteFragment = DetailNoteFragment.newInstance(note);
-//
-//            FragmentManager fragmentManager = getSupportFragmentManager();
-//            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-//            fragmentTransaction.replace(R.id.layout_container, detailNoteFragment);
-//            fragmentTransaction.commit();
-
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.layout_container, detailNoteFragment);
+            fragmentTransaction.commit();
+        }
     }
 }

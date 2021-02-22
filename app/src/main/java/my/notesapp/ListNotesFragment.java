9aff337a37ba23ff8264c9f1ebec4d3b76ad2b6e
SidorkinAlex <palach.orl@mail.ru>
2021-02-22 17:47:48 +0300

    добавлена Альбомная ориентация

--- app/src/main/java/my/notesapp/ListNotesFragment.java
+++ app/src/main/java/my/notesapp/ListNotesFragment.java
@@ -1,6 +1,7 @@
 package my.notesapp;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -19,15 +20,23 @@ import org.json.JSONObject;
 
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.concurrent.locks.Condition;
 
-public class ListNotes extends Fragment {
+public class ListNotesFragment extends Fragment {
     LinkedList<Note> notesList;
+    private boolean isLandOrientation;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list_notes, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        isLandOrientation = getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
     }
 
     @Override
@@ -38,11 +47,11 @@ public class ListNotes extends Fragment {
     }
 
     private void getNotesList() {
-        notesList= new LinkedList<Note>();
-        String s=getString(R.string.notes_mock);
+        notesList = new LinkedList<Note>();
+        String s = getString(R.string.notes_mock);
         try {
-            JSONArray jsonNotesList= new JSONArray(s);
-            for (int i = 0; i < jsonNotesList.length() ; i++) {
+            JSONArray jsonNotesList = new JSONArray(s);
+            for (int i = 0; i < jsonNotesList.length(); i++) {
                 JSONObject jsonNote = jsonNotesList.getJSONObject(i);
                 Note note = new Note(jsonNote);
                 notesList.add(note);
@@ -52,25 +61,43 @@ public class ListNotes extends Fragment {
         }
     }
 
-    private void initView(View view){
+    private void initView(View view) {
         LinearLayout list_note_body = (LinearLayout) view;
-        Iterator<Note> it = notesList. iterator() ;
-        while(it.hasNext()) {
+        Iterator<Note> it = notesList.iterator();
+        while (it.hasNext()) {
             Note note = it.next();
-            View rowNoteView = HelperNoteView.getRowNoteFromListView(note,view.getContext());
+            View rowNoteView = HelperNoteView.getRowNoteFromListView(note, view.getContext());
             rowNoteView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     System.out.println(" rowNoteView.setOnClickListener onClick");
-                    startDetailNote(note);
+                    checkOrientation(note);
                 }
             });
             list_note_body.addView(rowNoteView);
         }
     }
-    private void startDetailNote(Note note){
-        Intent intent = new Intent(getActivity(),DetailNoteActivity.class);
-        intent.putExtra(DetailNoteFragment.ARG_INDEX,note);
+
+    private void checkOrientation(Note note){
+        if (isLandOrientation){
+            startDetailNoteFragment(note);
+        } else {
+            startDetailNoteActivity(note);
+        }
+    }
+
+    private void startDetailNoteFragment(Note note){
+        DetailNoteFragment detailNoteFragment = DetailNoteFragment.newInstance(note);
+        requireActivity()
+                .getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.layout_container_land,detailNoteFragment)
+                .commit();
+    }
+
+    private void startDetailNoteActivity(Note note) {
+        Intent intent = new Intent(getActivity(), DetailNoteActivity.class);
+        intent.putExtra(DetailNoteFragment.ARG_INDEX, note);
         startActivity(intent);
     }
 }

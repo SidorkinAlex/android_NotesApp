424481a92e0a19ac18bea27bc1c1431c8a6e5d1e
SidorkinAlex <palach.orl@mail.ru>
2021-02-24 07:14:20 +0300

    обавлен фрагмент о приложении

--- app/src/main/java/my/notesapp/HelperRouter.java
+++ app/src/main/java/my/notesapp/HelperRouter.java
@@ -7,10 +7,14 @@ import com.google.android.material.navigation.NavigationView;
 public class HelperRouter {
     public static boolean routeActions(int id, MainActivity activity) {
         //логика обработки нажатия кнопок меню
-        switch (id){
+        switch (id) {
             case R.id.action_main:
                 ListNotesFragment listNotesFragment = new ListNotesFragment();
                 activity.replaceFragment(listNotesFragment);
+                return true;
+            case R.id.action_about:
+                AboutFragment aboutFragment  = new AboutFragment();
+                activity.replaceFragment(aboutFragment);
                 return true;
         }
         return false;
